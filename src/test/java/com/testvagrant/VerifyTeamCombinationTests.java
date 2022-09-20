fef8c7316c1ac2c0cee1b361f962ee5efefbb312
Hemant Tumbare <hemant_tumbare@hotmail.com>
2022-09-21 00:20:47 +0530

    updated test class to use logger

--- src/test/java/com/testvagrant/VerifyTeamCombinationTests.java
+++ src/test/java/com/testvagrant/VerifyTeamCombinationTests.java
@@ -2,6 +2,8 @@ package com.testvagrant;
 
 import java.util.Iterator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Assert;
@@ -12,6 +14,8 @@ import com.testvagrant.dataProviders.TestDataProvider;
 import com.testvagrant.utility.Utilities;
 
 public class VerifyTeamCombinationTests {
+	
+	private static final Logger logger = LogManager.getLogger(VerifyTeamCombinationTests.class);
 
 	@Test(dataProvider = "teamData", dataProviderClass = TestDataProvider.class)
 	public void verifyTeamHasFourForeignPlayers(Object teamName, Object dataFileName) {
@@ -19,7 +23,7 @@ public class VerifyTeamCombinationTests {
 		Utilities util = new Utilities();
 		int foreignPlayerCount = 0;
 
-		System.out.println("Reading data for team " + teamName + " from file : " + dataFileName);
+		logger.info("Reading data for team " + teamName + " from file : " + dataFileName);
 		JSONObject json = util.readJsonFile(Constants.DATA_FILE_LOCATION + dataFileName);
 
 		String actualTeamName = (String) json.get(Constants.FIELD_TEAM_NAME);
@@ -28,7 +32,7 @@ public class VerifyTeamCombinationTests {
 		JSONArray playersArray = (JSONArray) json.get(Constants.FIELD_PLAYER);
 		Iterator iterator = playersArray.iterator();
 
-		System.out.println("Looping through player list to count foreign players...!!!");
+		logger.info("Looping through player list to count foreign players...!!!");
 		while (iterator.hasNext()) {
 			JSONObject player = (JSONObject) iterator.next();
 			String playerCountry = (String) player.get(Constants.FIELD_PLAYER_COUNTRY);
@@ -38,7 +42,7 @@ public class VerifyTeamCombinationTests {
 		}
 		Assert.assertEquals("Foreign Player Count must be four...!!!", Constants.FOREIGN_PLAYER_COUNT_FOUR,
 				foreignPlayerCount);
-		System.out.println("Foreign Player Count in Team :" + foreignPlayerCount);
+		logger.info("Foreign Player Count in Team : "+teamName+ " is :"  + foreignPlayerCount);
 	}
 	
 
@@ -48,7 +52,7 @@ public class VerifyTeamCombinationTests {
 		Utilities util = new Utilities();
 		int wicketKeeper = 0;
 
-		System.out.println("Reading data for team " + teamName + " from file : " + dataFileName);
+		logger.info("Reading data for team " + teamName + " from file : " + dataFileName);
 		JSONObject json = util.readJsonFile(Constants.DATA_FILE_LOCATION + dataFileName);
 
 		String actualTeamName = (String) json.get(Constants.FIELD_TEAM_NAME);
@@ -57,7 +61,7 @@ public class VerifyTeamCombinationTests {
 		JSONArray playersArray = (JSONArray) json.get(Constants.FIELD_PLAYER);
 		Iterator iterator = playersArray.iterator();
 
-		System.out.println("Looping through player roles to count wicket-keepers...!!!");
+		logger.info("Looping through player roles to count wicket-keepers...!!!");
 		while (iterator.hasNext()) {
 			JSONObject player = (JSONObject) iterator.next();
 			String playerCountry = (String) player.get(Constants.FIELD_PLAYER_ROLE);
@@ -67,6 +71,6 @@ public class VerifyTeamCombinationTests {
 		}
 		Assert.assertTrue("Atleast one Wicket-Keeper must be present...!!!",
 				wicketKeeper >= Constants.MINIMUM_WICKET_KEEPER_COUNT);
-		System.out.println("Wicket Keeper count in team :" + wicketKeeper);
+		logger.info("Wicket Keeper count in Team :"+teamName+ " is :" + wicketKeeper);
 	}
 }
